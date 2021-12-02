b03e0fb9c442883d07bbef8a5746d326220be23d
prabhigupta <prabith7.g.s@gmail.com>
2021-12-02 12:23:31 +0530

    added cpp for day1 and day2

---
+++ day01/CPP/day01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string command = "./binary/day01A";
+    system(command.c_str());
+    command = "./binary/day01B";
+    system(command.c_str());
+    return 0;
+}
